b8b6c6a3fbb903de69b2a43b3d5af650d6ac00a7
kanphis <kanphis@gmail.com>
2014-12-03 22:48:49 +0300

    Some changes of my modules.

---
+++ app/src/main/java/com/project/a_team/wakeupper/UnblockActivity.java
@@ -0,0 +1,13 @@
+package com.project.a_team.wakeupper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnblockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unblock);
+    }
+}
